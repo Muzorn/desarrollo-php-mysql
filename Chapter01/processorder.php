7f9d04360fb67a5a9779568743b8c12c78003cee
Muzorn <jesusmgc.it@gmail.com>
2019-04-03 14:32:46 +0200

    Agregamos contenido dinámico: fecha en que se procesa el fichero PHP en cuestión.

--- Chapter01/processorder.php
+++ Chapter01/processorder.php
@@ -15,6 +15,16 @@
   <body>
         <h1>Bob's Auto Parts</h1>
         <h2>Order Results</h2>
-        <?php echo '<p>Order processed.</p>'; ?>
+        <?php
+            echo "<p>Order processed at ";
+            echo date('H:i, jS F Y');
+            echo "</p>"
+        ?>
+        <?php
+        /**
+         * Versión en una línea:
+         * echo "<p>Order processed at " . date('H:i, jS F Y') . "</p>";
+         */
+        ?>
   </body>
 </html>
