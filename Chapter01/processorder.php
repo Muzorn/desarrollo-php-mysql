277556f0e64dce2260389fb80316230d16853219
Muzorn <jesusmgc.it@gmail.com>
2019-04-03 13:34:22 +0200

    Creamos el formulario de procesamiento de pedidos y el PHP inicial donde lo procesaremos.

---
+++ Chapter01/processorder.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarlaxxe
+ * Date: 03/04/2019
+ * Time: 13:31
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Bob's Auto Parts - Order Form</title>
+  </head>
+  <body>
+        <h1>Bob's Auto Parts</h1>
+        <h2>Order Results</h2>
+  </body>
+</html>
