9aaccf32c7cef3e4c527a3e620feab101ab88737
Muzorn <jesusmgc.it@gmail.com>
2019-04-06 16:58:56 +0200

    Refactorizamos la tabla de gastos de envío para generar la estructura de filas con un bucle, eliminando así código HTML tedioso y repetido innecesariamente.

--- Chapter01/freight.php
+++ Chapter01/freight.php
@@ -13,30 +13,22 @@
   </head>
   <body>
     <table style="border: 0px; padding: 3px">
-    <tr>
-     <td style="background: #cccccc; text-align: center;">Distance</td>
-     <td style="background: #cccccc; text-align: center;">Cost</td>
-    </tr>
-    <tr>
-     <td style="text-align: right;">50</td>
-     <td style="text-align: right;">5</td>
-    </tr>
-    <tr>
-     <td style="text-align: right;">100</td>
-     <td style="text-align: right;">10</td>
-    </tr>
-    <tr>
-     <td style="text-align: right;">150</td>
-     <td style="text-align: right;">15</td>
-    </tr>
-    <tr>
-     <td style="text-align: right;">200</td>
-     <td style="text-align: right;">20</td>
-    </tr>
-    <tr>
-     <td style="text-align: right;">250</td>
-     <td style="text-align: right;">25</td>
-    </tr>
+        <tr>
+            <td style="background: #cccccc; text-align: center;">Distance</td>
+            <td style="background: #cccccc; text-align: center;">Cost</td>
+        </tr>
+        <?php
+            $distance = 50;
+
+            while ($distance <= 250) {
+                echo "<tr>";
+                echo "<td>$distance</td>";
+                echo "<td>" . ($distance/10) . "</td>";
+                echo "<tr>";
+
+                $distance += 50;
+            }
+        ?>
     </table>
   </body>
 </html>
