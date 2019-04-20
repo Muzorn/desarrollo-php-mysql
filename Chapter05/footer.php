ec50de62934c083bb3db99fb16b6da22a917a239
Muzorn <jesusmgc.it@gmail.com>
2019-04-20 15:32:50 +0200

    Creamos una página con cabecera y footer genéricos mediante inclusión de require() pero siguiendo un poco la convención más correcta para mí en lugar de la forma del libro: en vez de meter parte de la estructura HTML en el header.php, el esqueleto HTML principal pasa al home.php y ahí es donde vamos incluyendo las distintas partes que forman la web (un poco como haríamos con Twig en Symfony de tener una plantilla base con un esqueleto).

---
+++ Chapter05/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarlaxxe
+ * Date: 20/04/2019
+ * Time: 15:23
+ */
+
+?>
+
+<footer>
+    <p>&copy; TLA Consulting Pty Ltd.<br />
+        Please see our
+        <a href="legal.php">legal information page</a>.</p>
+</footer>
---
+++ Chapter05/home.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarlaxxe
+ * Date: 20/04/2019
+ * Time: 15:21
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TLA Consulting Pty Ltd</title>
+    <link href="styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <!-- Page header -->
+    <?php require ('header.php'); ?>
+
+    <!-- page content -->
+    <section>
+        <h2>Welcome to the home of TLA Consulting.</h2>
+        <p>Please take some time to get to know us.</p>
+        <p>We specialize in serving your business needs
+            and hope to hear from you soon.</p>
+    </section>
+
+    <!-- Page footer -->
+    <?php require ('footer.php'); ?>
+</body>
+</html>
