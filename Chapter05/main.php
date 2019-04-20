6a51020f95b59fc4be584d9d9d1cc9a961a4d787
Muzorn <jesusmgc.it@gmail.com>
2019-04-20 15:17:17 +0200

    Ejemplo básico de inclusión de archivos PHP mediante require().

---
+++ Chapter05/main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jarlaxxe
+ * Date: 20/04/2019
+ * Time: 15:12
+ */
+
+echo "This is the main file. <br/>";
+require ('reusable.php');
+echo "The script will end now. <br/>";
